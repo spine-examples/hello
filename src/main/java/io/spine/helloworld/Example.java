5c181f4f38757cd2da884bb08b01ed2f4aac16b9
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2020-08-05 14:22:37 +0300

    Document warning suppression

--- src/main/java/io/spine/helloworld/Example.java
+++ src/main/java/io/spine/helloworld/Example.java
@@ -70,8 +70,7 @@ public final class Example {
      * @see Server
      * @see Client
      */
-    @SuppressWarnings("UnstableApiUsage") /* Uninterruptibles.sleepUninterruptibly() from Guava
-        is still @Beta. It's safe to use for this example. */
+    @SuppressWarnings("UnstableApiUsage") // `sleepUninterruptibly()` is @Beta. OK for this example.
     public static void main(String[] args) {
         String serverName = UUID.randomUUID().toString();
         Server server = new Server(serverName);
